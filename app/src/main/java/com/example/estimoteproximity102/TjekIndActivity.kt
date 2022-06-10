1aff6c0a9c5075dde6deff3571015aa0cccf5cab
SilleMcgregerse <sille.gregersen@hotmail.dk>
2022-06-10 13:24:32 +0200

    Basic sideopsætning

--- app/src/main/java/com/example/estimoteproximity102/TjekIndActivity.kt
+++ app/src/main/java/com/example/estimoteproximity102/TjekIndActivity.kt
@@ -1,43 +1,103 @@
-package com.example.estimoteproximity102
+package com.example.estimoteproximity102.ui.theme
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.estimoteproximity102.ui.theme.EstimoteProximity102Theme
+import androidx.compose.ui.unit.dp
+import com.example.estimoteproximity102.R
 
-class TjekIndActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            EstimoteProximity102Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+@Preview(showSystemUi = true)
+@Composable
+fun TjekIndActivity(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        Title()
+        OpgaveListe()
+        TjekIndButton()
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Title() {
+    Text(
+        "BorgerNavn"
+    )
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview2() {
-    EstimoteProximity102Theme {
-        Greeting("Android")
-    }
+fun OpgaveListe() {
+    Text("OpgaveListe"
+    )
 }
+
+@Preview
+@Composable
+fun TjekIndButton() {
+    Text("TjekIndButton"
+    )
+}
+
+//@Composable fun Title(){
+//    Text(
+//        text = stringResource(R.string.sign_in_welcome_text)
+//    )
+//}
+//@Composable fun SignInButton(){
+//    Button(
+//        onClick = {},
+//        modifier = Modifier.fillMaxWidth(),
+//        contentPadding = PaddingValues(24.dp),
+//        colors = ButtonDefaults.buttonColors(
+//            backgroundColor = Color.Gray,
+//            contentColor=Color.White
+//        )
+//    ) {
+//        Text(
+//            text= stringResource(id = (R.string.sign_in))
+//        )
+//    }
+//}
+//@Composable fun Password(){
+//    val passwordState = remember { mutableStateOf(TextFieldValue())}
+//    TextField(
+//        modifier = Modifier.fillMaxWidth(),
+//        value = passwordState.value, onValueChange ={passwordState.value=it},
+//        label = {Text(text= stringResource(R.string.kodeord))},
+//        colors=TextFieldDefaults.textFieldColors(
+//            focusedIndicatorColor = Color.Transparent,
+//            unfocusedIndicatorColor = Color.Transparent
+//        ),
+//        shape = RoundedCornerShape(9.dp)
+//    )
+//}
+//@Composable fun Email(){
+//    val emailState = remember { mutableStateOf(TextFieldValue())}
+//    TextField(
+//        modifier = Modifier.fillMaxWidth(),
+//        value = emailState.value,
+//        onValueChange ={emailState.value=it},
+//        label = {Text(text= stringResource(R.string.email))},
+//        colors=TextFieldDefaults.textFieldColors(
+//            focusedIndicatorColor = Color.Transparent,
+//            unfocusedIndicatorColor = Color.Transparent
+//        )
+//    )
+//}
