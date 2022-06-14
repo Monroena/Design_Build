532d9d7d9f88a8a147c5ec26bea3781224dbc471
SilleMcgregerse <sille.gregersen@hotmail.dk>
2022-06-14 14:06:00 +0200

    TjekInd side uden funktionalitet-Sille

---
+++ app/src/main/java/com/example/estimoteproximity102/TjekInd.kt
@@ -0,0 +1,61 @@
+package com.example.estimoteproximity102
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.estimoteproximity102.ui.theme.EstimoteProximity102Theme
+
+
+@Preview(showBackground = true)
+@Composable
+fun TjekIndPreview() {
+    EstimoteProximity102Theme {
+        TjekInd()
+    }
+}
+
+@Composable
+fun TjekInd() {
+    var BorgerNavn by remember { mutableStateOf("") }
+    var BorgerOpgaveliste by remember { mutableStateOf("")}
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.padding(horizontal = 10.dp)
+    ) {
+
+        OutlinedTextField(
+            value = BorgerNavn,
+            onValueChange = { BorgerNavn = it },
+            label = { Text(stringResource(R.string.borgerNavn)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = BorgerOpgaveliste,
+            onValueChange = { BorgerOpgaveliste = it },
+            label = { Text(stringResource(R.string.borgerOpgaveliste))},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = {
+                Toast.makeText(
+                    context,
+                    "Du er tjekket ind",
+                    Toast.LENGTH_LONG
+                ).show()
+            }) {
+            Text(text = "Tjek Ind")
+        }
+    }
+}
